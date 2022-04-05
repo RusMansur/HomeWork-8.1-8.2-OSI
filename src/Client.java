9982f52508e826dc2fa45c70e54c1ee94e799978
Руслан Долгополов <rusdolg@gmail.com>
2022-04-05 16:47:24 +0300

    Первоначальная версия

--- src/Client.java
+++ src/Client.java
@@ -3,22 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        String host = "localhost";
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String host = "netology.homework";
         int port = 8089;
-
-        try (Socket clientSocket = new Socket(host,port)) {
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+        boolean connectEstablished = true;
+            Socket clientSocket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            out.println("Netology");
-
-            String resp = in.readLine();
-            System.out.println(resp);
-        } catch (IOException exception) {
-            exception.printStackTrace();
+        while (connectEstablished) {
+            String response = in.readLine();
+            System.out.println(response);
+            String string = input.nextLine();
+            if (string.equals("exit")) {
+                connectEstablished = false;
+                out.println(string);
+                response = in.readLine();
+                System.out.println(response);
+            }
+            out.println(string);
         }
+        clientSocket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -5,21 +5,37 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import static java.lang.System.out;
+
 public class Server {
 
     public static void main(String[] args) throws IOException {
         int port = 8089;
-
+        System.out.println("Сервер запущен...");
         while (true) {
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket connection = serverSocket.accept();
-                PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket connection = serverSocket.accept();
 
-                System.out.println("Новое соединение установлено");
-                final String name = in.readLine();
-                out.println(String.format("Привет %s, ваш порт %d", name, connection.getPort()));
-                serverSocket.close();
+            PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            out.println("Добрый день! Как ваше имя?");
+            final String username = in.readLine();
+            System.out.format("Пользователь %s в сети...\n", username);
+            out.println(String.format("Привет, %s! Вам уже есть 18 лет? Напишите 'да (yes)'/'нет (no)' или 'exit', чтобы отключиться...", username));
+            while (true) {
+                String answer = in.readLine();
+                if (answer.equals("да") || answer.equals("yes")) {
+                    out.println(String.format("%s, добро пожаловать в раздел для взрослых. Хорошего отдыха или приятной работы!", username));
+                } else if (answer.equals("нет") || answer.equals("no")) {
+                    out.println(String.format("%s, добро пожаловать в раздел для детей. Давай поиграем!", username));
+                } else if (answer.equals("exit")) {
+                    out.println("Всего доброго! Хорошего дня! Сессия завершена!");
+                    System.out.printf("Пользователь %s вышел из сети.\n", username);
+                    break;
+                }
+            }
+            serverSocket.close();
         }
     }
 }
